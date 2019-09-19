208c704bba7132c0410e9b96cd935d4dd72d829b
Henry <henrstep@amazon.com>
2019-09-19 15:45:44 -0400

    update js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,72 @@
 (function(){
+    function sum(number1, number2){
+        // console.log(number1 + number2)
+
+    }
+    sum(5,9)
+    sum(13,8)
+
+    //.............................................
+
+    function avg(someNumber1, someNumber2, someNumber3){
+        // console.log((someNumber1 + someNumber2 + someNumber3) / 3)
+    }
+
+    avg(1, 5, 3)
+
+    //..............................................
+
+    function greaterThan(number3, number4){
+        if (number4 > number3){
+            return true
+        }
+        else{
+            return false
+        }
+            
+    }
+    // console.log(greaterThan(3, 3540))
+    // console.log(greaterThan(4000, 45))
+
+    //................................................
+
+    function secondLargest(){
+        var array = [4, 2, 10];
+            array.sort(function(a, b) {
+                return a - b;
+            });
+            
+            return array[array.length - 2]
+    }
+
+        // console.log(secondLargest())
+
+    //..................................................
     
+    function containsVowel(word){
+        var vowelList = "alpha" 
+        var result = word.indexOf(vowelList)
+
+        // return result 
+
+        if(result == -1){
+            return false
+        }else {
+            return true
+        }
+
+    }
+        // console.log(containsVowel("alpha"))
+
+    //.....................................................
+
+
+    function pigLatin(hello){
+        
+    }
+
+
+
 
 
 
